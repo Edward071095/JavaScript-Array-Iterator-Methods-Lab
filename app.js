d4a696671b8224295f1a8d30edbb7678f613a371
EdwardW710 <EdwardW710@yahoo.com>
2025-01-30 16:50:24 -0500

    iterator ex 4

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ Exercise 3: Array.prototype.sort()
 Sort the inventors by birth date in ascending order (from those born furthest in
 the past to those born most recently).
 */
-
+/*
 const inventors = [
     { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
     { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -142,4 +142,51 @@ console.log('Exercise 3 correct result: ', [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
 ]);
+*/
+
+
+
+/* 
+Exercise 4: Array.prototype.find()
+
+Use Array.prototype.find() to identify the inventor with the first name 'Ada' 
+from an array of inventor objects
+
+- You have an array of objects, each representing an inventor with various 
+  properties including their first name.
+- Utilize the Array.prototype.find() method to locate the object for the 
+  inventor with the first name 'Ada'.
+- Assign the found inventor object to the variable inventorNamedAda
+*/
+
+
+
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+  ];
+
+
+let inventorNamedAda = inventors.find(inventor => inventor.first === 'Ada');
+
+// Complete the exercise in the space below:
+
+// Check your work:
+console.log('Exercise 4 my result: ', inventorNamedAda);
+console.log('Exercise 4 correct result: ', {
+  first: 'Ada',
+  last: 'Lovelace',
+  year: 1815,
+  passed: 1852,
+});
 
